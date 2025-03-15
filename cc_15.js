1b33353e6d8c7df37745b277b94fe67bb7b4fa92
EastonMullins <eastonmullins@usf.edu>
2025-03-15 16:00:32 -0400

    Task 2 -Adding Risk Items Dynamically

--- cc_15.js
+++ cc_15.js
@@ -2,4 +2,24 @@
 //Completed in HTML
 
 
+// Task 2 - Adding Risk Items Dynamically
 
+function addRiskItem(riskName, riskLevel, department) {
+    const riskContainer = document.getElementById("riskDashboard");
+
+    const riskCard = document.createElement("div");
+    riskCard.setAttribute("class","risk-card");
+
+    const nameElement = document.createElement("h2");
+    nameElement.textContent = riskName;
+
+    const levelElement = document.createElement("p");
+    levelElement.textContent = riskLevel;
+
+    const departmentElement = document.createElement("span");
+    departmentElement.textContent = department;
+}
+// Test Cases
+addRiskItem("Lost Checks", "High", "Finance");
+addRiskItem("Customer Contract Negotiatons", "Medium", "Marketing");
+addRiskItem("Employee Retention", "Low", "Human Resources");
